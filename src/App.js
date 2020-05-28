d8079049cd575e376d37f73bb11f59a2f1f2e454
MurrTuck <murrtuck@gmail.com>
2020-05-28 12:32:26 -0600

    Mini Complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
+import { HasRouter } from 'react-router-dom'
 import Nav from "./Components/Nav/Nav";
+import router from './router'
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
+      //brought it into App.js, it's usually in the index.js file. 
+      <HasRouter>
+        <div className="App">
+          <Nav />
+          {router}
+        </div>
+      </HasRouter>
     );
   }
 }
